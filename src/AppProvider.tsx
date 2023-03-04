c0a422997607d63556c3f2019b551d76e03f27b9
bbachain <bbachain123@gmail.com>
2023-03-04 17:25:16 +0700

    added paper provider

--- src/AppProvider.tsx
+++ src/AppProvider.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
-import {Provider} from 'react-redux';
+import {Provider as StoreProvider} from 'react-redux';
+import {
+  MD3LightTheme as DefaultTheme,
+  Provider as PaperProvider,
+} from 'react-native-paper';
+
 import {store} from './store';
 
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'tomato',
+    secondary: 'yellow',
+  },
+};
+
 const AppProvider = ({children}: any) => {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <StoreProvider store={store}>
+      <PaperProvider theme={theme}>{children}</PaperProvider>
+    </StoreProvider>
+  );
 };
 
 export default AppProvider;
