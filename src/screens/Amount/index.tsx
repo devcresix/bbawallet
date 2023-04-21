67b2e30dea2cceb6ed66d810aba2b3f508b424c4
Philip James <bbachain123@gmail.com>
2023-04-21 11:41:18 +0300

    fix: receive and send + scan qrcode

--- src/screens/Amount/index.tsx
+++ src/screens/Amount/index.tsx
@@ -1,27 +1,45 @@
 import React, {useEffect, useState} from 'react';
+import {Text} from 'react-native-paper';
 
 import TextInput from '../../components/Input';
 import Button from '../../components/Button';
 
 import {withTranslation} from '../../hooks/useTranslations';
 import useAccounts from '../../hooks/useAccounts';
+import useNetworks from '../../hooks/useNetworks';
 
-function AmountScreen({route, _navigation, t}: any) {
+function AmountScreen({route, navigation, t}: any) {
   const {destination} = route.params;
+  const {network} = useNetworks();
   const {account} = useAccounts();
   const [receiver, setReceiver] = useState('');
+  const [balance, setBalance] = useState('0');
   const [amount, setAmount] = useState('0');
 
   useEffect(() => {
     if (destination) {
       setReceiver(destination);
+      account.getBalance().then(bal => setBalance(`${bal}`));
     }
-  }, [destination]);
+  }, [account, destination]);
 
-  const handlePressNext = () => {};
   const handlePressMax = async () => {
-    const balance = await account.getBalance();
-    console.log('Loaded Max Amount:', balance);
+    const estimated = await account.estimateMaxTransfer(receiver);
+    setAmount(`${estimated}`);
+  };
+
+  const handlePressNext = async () => {
+    try {
+      const transaction = await account.createTransaction(
+        receiver,
+        parseFloat(amount),
+      );
+      const txid = await account.sendTransaction(transaction);
+      console.log(txid);
+      navigation.push('Home');
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -42,10 +60,17 @@ function AmountScreen({route, _navigation, t}: any) {
         right="check-all"
         rightOnPress={handlePressMax}
       />
+      <Text>
+        Available: {balance} {network.symbol}
+      </Text>
+      {/* <Text>
+        Network Fee: {balance} {network.symbol}
+      </Text> */}
+
       <Button
         mode="contained"
         icon="page-next"
-        title={t('common.next')}
+        title={t('common.sign')}
         onPress={handlePressNext}
       />
     </>
